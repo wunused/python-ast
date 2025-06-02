e7ffe523bde053caddc79c19e882d5d1b37c1a8f
HassamGani <hag2137@columbia.edu>
2025-06-02 10:13:49 -0400

    lots of changes; working to make it so it analyzes each import statement first, goes to the module, analyzes import, goes to module and so on and after it reaches last module, it starts analyzing the components besides the import statements from there. main work right now is with the import classes

--- analyzer.py
+++ analyzer.py
@@ -1,13 +1,12 @@
 import ast
-from pprint import pprint
 
 instanceList: list[str] = []
 analyzedModule = set()
 analyzedClass = set()
 
 def main():
-    firstFileName = input("Insert file: ") #just for when it first runs, asks the user for which file they want to analyze
-    analyze(firstFileName) #calls the analyze function with said file as argument
+    firstFileName = input("Insert file: ") 
+    analyze(firstFileName)
     
     for instance in instanceList:
         print(instance)
@@ -18,9 +17,8 @@ def main():
                     print(instance)
 
 class Analyzer(ast.NodeVisitor):
-    def __init__(self): #constructor
-        pass
-    #we only want to check for classes, the classes they inherit from, functions, their imports, and do the same in the modules they import
+    def __init__(self, fileName):
+        self.upperModule = moduleInfo(fileName)
     def visit_ClassDef(self, node):
         newClassDefInfo = ClassDefInfo()
         newClassDefInfo.name = node.name
@@ -35,8 +33,39 @@ class Analyzer(ast.NodeVisitor):
         for method in node.body:
             newClassDefInfo.methods.append(method)
 
+    def visit_Import(self, node):
+        importInfoBuilder(self, node)
+        self.generic_visit(node)
+
+    def visit_ImportFrom(self, node):
+        importInfoBuilder(self, node)
+        self.generic_visit(node)
+
+def importInfoBuilder(analyzer, node):
+    for alias in node.names:
+            upperImportInfo = importInfo(
+                alias.name, 
+                getattr(alias, "asname", None), 
+                getattr(node, "module", None))
+        
+            analyzer.upperModule.imports.append(upperImportInfo)
+
+class moduleInfo():
+    def __init__(self, name):
+        self.name: str = name
+        self.imports: importInfo[list] = []
+        self.classes: ClassDefInfo[list] = []
+        self.methods: methodDefInfo[list] = []
+
+class importInfo():
+    def __init__(self, name, asname, fromInfo):
+        if fromInfo != None:
+            self.fromInfo = self.module
+        self.name: str = name # the name of the imported thing
+        self.asname: str = asname # the asname of the imported thing
+        self.module: moduleInfo
+        
 class ClassDefInfo():
-    
     def __init__(self):
         instanceList.append(self)
         self.moduleFileNames: list[str] = []
@@ -56,23 +85,28 @@ class ClassDefInfo():
             f")"
         )
 
-def getName(base): #each base field could belong to a different class
-    if isinstance(base, ast.Name): #check if current base field belongs to Name class in ast (refers to a top level class, the module itself, meaning it goes no deeper than that via attributes)—essentially checking if inherits from something without attributes
-        return base.id #return name (stored as id) of class
-    if isinstance(base, ast.Attribute): #check if current base field belongs to an attribute
-        return getName(base.value) + "." + base.attr #if so, get the class it comes from and itself, the attribute. when dealing with inheritance from attributes, the top level class name is stored in the attribute "id" of the "value" field/attribute
+class methodDefInfo:
+    def __init__(self):
+        pass
+def getName(base):
+    if isinstance(base, ast.Name): 
+        return base.id 
+    if isinstance(base, ast.Attribute): 
+        return getName(base.value) + "." + base.attr 
         
 def analyze(fileName):
-        if fileName in analyzedModule: #when re-checking the list of modules, we dont want it to reanalyze the first infinitely
+        if fileName in analyzedModule: 
             return
-        with open(fileName, "r") as source: #opens the file passed in in readable mode; stores it as source variable
-            tree = ast.parse(source.read()) #.read() turns the file's content into a string that the ast module can invoke the parse method on
-        analyzer = Analyzer() #new instance of Analyzer()
-        analyzer.visit(tree) #analyzer visits the tree; visit method looks for each node to then invoke visit_(NodeType) on it; if doesn't find visit_(NodeType) declared in the Analyzer class, it calls self.generic_visit(node), which goes into the next layer and repeats the process
+        
+        with open(fileName, "r") as source: 
+            tree = ast.parse(source.read()) 
+        analyzer = Analyzer(fileName) 
+        upperModule = moduleInfo(fileName)
+        analyzer.visit(tree) 
         analyzedModule.add(fileName)
         
         for classInstance in instanceList:
-            if classInstance.name not in analyzedClass: #when re-checking the classes to find the modules, though the modules will shave been safely added to the analyzedModule set to not go infinitely, this ensures it doesn't even have to recheck a class whose all modules have been analyzed, saving time
+            if classInstance.name not in analyzedClass: 
                 for element in classInstance.moduleFileNames:
                     analyze(element)
                 analyzedClass.add(classInstance.name)
