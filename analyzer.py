1bc86df446bc05a8d355c7b7b1c67699a08c5854
HassamGani <hag2137@columbia.edu>
2025-06-03 16:49:23 -0400

    unfinished; lots of changes done but still roughly making main idea come into being

--- analyzer.py
+++ analyzer.py
@@ -20,8 +20,7 @@ class Analyzer(ast.NodeVisitor):
     def __init__(self, fileName):
         self.upperModule = moduleInfo(fileName)
     def visit_ClassDef(self, node):
-        newClassDefInfo = ClassDefInfo()
-        newClassDefInfo.name = node.name
+        newClassDefInfo = ClassDefInfo(node.name, self.upperModule.imports)
         for base in node.bases: #bases refer to classes they inherit from; could be multiple, which is why it is a for loop
             baseName = getName(base) #gets name of the bases
             newClassDefInfo.base_names.append(baseName)
@@ -47,15 +46,24 @@ def importInfoBuilder(analyzer, node):
                 alias.name, 
                 getattr(alias, "asname", None), 
                 getattr(node, "module", None))
-        
+
             analyzer.upperModule.imports.append(upperImportInfo)
 
+# Dictionary for Lookup:
+
+master_dictionary = {"modules": []}
+
+# CLASSES FOR NODE TYPES:
+
+# Module Node:
 class moduleInfo():
     def __init__(self, name):
         self.name: str = name
-        self.imports: importInfo[list] = []
-        self.classes: ClassDefInfo[list] = []
-        self.methods: methodDefInfo[list] = []
+        self.imports: list[importInfo] = [] # use dictionaries to look up by name
+        self.classes: list[ClassDefInfo] = []
+        self.methods: list[methodDefInfo] = []
+
+# Import Node:
 
 class importInfo():
     def __init__(self, name, asname, fromInfo):
@@ -64,14 +72,17 @@ class importInfo():
         self.name: str = name # the name of the imported thing
         self.asname: str = asname # the asname of the imported thing
         self.module: moduleInfo
-        
+
+# Class Node:
+
 class ClassDefInfo():
-    def __init__(self):
+    def __init__(self, name, importInfoList):
+        for i in importInfoList:
+            if i.module.name + i.asname == self.inherited_classes_fullNames or i.asname == self.inherited_classes_fullNames:
+                self.inherited_classes_fullNames = i.module.name + i.name
         instanceList.append(self)
-        self.moduleFileNames: list[str] = []
-        self.name: str
-        self.base_names: list[str] = []
-        self.inherited_classes: list[str] = []
+        self.name: str = name
+        self.inherited_classes_fullNames: list[str] = []
         self.methods: list[str] = []
 
     def __repr__(self):
@@ -85,10 +96,25 @@ class ClassDefInfo():
             f")"
         )
 
+# Method Node:
+
 class methodDefInfo:
     def __init__(self):
         pass
 def getName(base):
+    
+    # some checker for whether or not inherited class was imported (rough sketch)
+        
+        #if it is inherited
+
+            # if it comes as a full name (module.class)
+
+                # if it comes as alias.class
+            
+            #if it comes as a class
+
+                #if the class is an alias
+    
     if isinstance(base, ast.Name): 
         return base.id 
     if isinstance(base, ast.Attribute): 
@@ -100,8 +126,7 @@ def analyze(fileName):
         
         with open(fileName, "r") as source: 
             tree = ast.parse(source.read()) 
-        analyzer = Analyzer(fileName) 
-        upperModule = moduleInfo(fileName)
+        analyzer = Analyzer(fileName)
         analyzer.visit(tree) 
         analyzedModule.add(fileName)
         
