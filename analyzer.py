d24a5f2b685cb8e068db8a61db6caecede4d9325
HassamGani <hag2137@columbia.edu>
2025-06-12 15:55:25 -0400

    working on formatting single-argument output

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,6 @@
 import ast
 import cli
+import cliLogic
 
 global_dictionary = {"modules_dictionary": {},
                     "classes_dictionary": {},
@@ -14,9 +15,11 @@ def main ():
     # For example:
     # python3 analyzer.py app.py "app.A"
     
-    moduleName = cli.args.file.rsplit("/")[-1].split(".")[0]
+    moduleName = args.file_name.rsplit("/", 1)[-1].split(".")[0]
     #moduleName = input("Insert file name: ").split(".")[0]
     masterAnalyzer(moduleName)
+    
+    cli.logic()
 
     # TODO: Print out the analyzed class's inheritance information.
     #
@@ -53,13 +56,9 @@ class subAnalyzer(ast.NodeVisitor):
         self.generic_visit(node)
         self.highestLevel = self.previousLevel
 
-    """Make it so that it can store the FunctionInfo()
-    object into the class it's in"""
-
     def visit_FunctionDef(self, node):
         functionInfoBuilder(self, node)
         self.generic_visit(node)
-
 
 class moduleInfo():
     def __init__(self, name):
@@ -71,15 +70,14 @@ class moduleInfo():
     def __repr__(self):
         return (
             f"Module Name = {self.name}\n"
-            f"Module Imports = {self.imports}\n"
-            f"Module Classes = {self.classes}\n"
+            f"Module Imports =\n{self.imports}\n"
+            f"Module Classes =\n{self.classes}\n"
         )
 
 def functionInfoBuilder(analyzer, node):
     analyzer.highestLevel.functions[node.name] = functionInstance = FunctionInfo(node.name, analyzer.highestLevel)
     global_dictionary["functions_dictionary"][functionInstance] = functionInstance
 
-    # Find a way to separate from innate methods and inherited ones
 class FunctionInfo():
     def __init__(self, name, parent):
         self.name = name
@@ -87,44 +85,22 @@ class FunctionInfo():
             self.parentType = "Class"
         else:
             self.parentType = "Module"
-        self.parent = parent # we're going to need this to later check if a function is inherited or not
+        self.parent = parent
 
 def classInfoBuilder(analyzer, node):
-
-    # MISSING: Create a case for when it inherits from class within same module
-
     analyzer.highestLevel.classes[analyzer.highestLevel.name + "." + node.name] = classInstance = ClassInfo(analyzer.highestLevel.name + "." + node.name)
     global_dictionary["classes_dictionary"][classInstance.name] = classInstance
 
-    # there should be 2 possibilities here: 1 - inherited class is from another module
-    """# 2 - inherited class is from same module. because classes cant inherit from
-    classes defined after them in the same module, the inherited class has always
-    already been analyzed"""
-
-    for base in node.bases: #bases refer to classes they inherit from; could be multiple, which is why it is a for loop
+    for base in node.bases: 
         fullName = asname_to_name(analyzer, getFullName(base))
         if "." not in fullName:
             if fullName in analyzer.highestLevel.imports:
                 fullName = analyzer.highestLevel.imports[fullName].module.name + "." + fullName
             else:
-                fullName = analyzer.highestLevel.classes[analyzer.highestLevel.name
-                                                        + "."
-                                                        + fullName].name
-        # find a way to be able to tell whether or not this comes from the same module or different one
-
+                fullName = analyzer.highestLevel.classes[analyzer.highestLevel.name + "." + fullName].name
         classInstance.inherited_classes[fullName] = global_dictionary["classes_dictionary"][fullName]
         classInstance.inherited_functions[f"Inherited from: {fullName}"] = classInstance.inherited_classes[fullName].functions
     return classInstance
-
-    """
-    by the rule of classes, if every inherited
-    class has already been analyzed, then all
-    their functions have also been analyzed.
-
-    Therefore, whenever we analyze any class, we
-    can immediately attach inherited functions into
-    an inherited_functions attribute
-    """
 
 def asname_to_name(analyzer, formerName):
     for key in analyzer.highestLevel.imports.values():
@@ -138,20 +114,6 @@ def getFullName(base):
         return base.id
     if isinstance(base, ast.Attribute):
         return getFullName(base.value) + "." + base.attr
-
-    """
-    if we agree that it starts at the very last possible
-    module without an import, meaning the following non-import
-    parts of the modules will always inherit from an analyzed class,
-    every base found will have already been analyzed.
-
-    That means we need to find a way to search for the existing class
-    through the base and add them to a key in an inherited_classes
-    dictionary
-
-    Only possible exception is when a class inherits from a class
-    defined in the same module
-    """
 
 class ClassInfo():
     def __init__(self, name):
@@ -188,13 +150,33 @@ class importInfo():
             self.module: moduleInfo = global_dictionary["modules_dictionary"][name]
 
     def __repr__(self):
-        return (
-            f"Import Type = {self.type}\n"
-            f"Real Name = {self.name}\n"
-            f"User-generated Name = {self.asname}\n"
-            f"Module Name = {self.module.name}\n"
-        )
+        if self.asname is not None:
+            if self.type == "object":
+                return (
+                    f"\nReal Name = {self.name}\n"
+                    f"User-generated Name = {self.asname}\n"
+                    f"Import Type = {self.type}\n"
+                    f"Module = {self.module.name}"
+                )
+            else:
+                return(
+                    f"\nReal Name = {self.name}\n"
+                    f"User-generated Name = {self.asname}\n"
+                    f"Import Type = {self.type}\n"
+                )
+        else:
+            if self.type == "object":
+                return (
+                    f"\nName = {self.name}\n"
+                    f"Import Type = {self.type}\n"
+                    f"Module = {self.module.name}"
+                )
+            else:
+                return(
+                    f"\nName = {self.name}\n"
+                    f"Import Type = {self.type}\n"
+                )
 
-cli.args
-topPath = cli.args.file.rsplit("/", 1)[0]
+args = cli.parser.parse_args()
+topPath = args.file_name.rsplit("/", 1)[0]
 main()
