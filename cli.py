662e66c381597688483ca9887f9946d1a52cffe1
HassamGani <hag2137@columbia.edu>
2025-06-12 15:55:25 -0400

    working on formatting single-argument output

--- cli.py
+++ cli.py
@@ -1,5 +1,15 @@
 import argparse
+from pprint import pprint
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file", help="analyzes inserted python file")
-args = parser.parse_args()
+parser.add_argument("file_name", help="analyzes inserted python file")
+parser.add_argument("-class_name", help="provides details for a class | Usage: module.class")
+
+def logic():
+    import analyzer
+    from analyzer import args
+    non_file_name_args = {k: v for k, v in vars(args).items() if k != "file_name" and v not in (None, False)}
+    if not non_file_name_args:
+        pprint(analyzer.global_dictionary["modules_dictionary"][args.file_name.rsplit("/", 1)[-1].split(".")[0]])
+        
+#args = parser.parse_args()
