5572686532f54d54290e5e7d1d61ff20386df696
HassamGani <hag2137@columbia.edu>
2025-06-14 15:11:11 -0400

    successfully takes in classes as optional argument

--- cli.py
+++ cli.py
@@ -8,7 +8,8 @@ global_dictionary = {"modules_dictionary": {},
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file_name", help="analyzes inserted python file")
-parser.add_argument("-class_name", help="provides details for a class | Usage: module.class")
+parser.add_argument("-c", "--class_name", help="provides details for a class | Usage: module.class")
+parser.add_argument("-f", "--function_name", help="provides details for a function")
 
 args = parser.parse_args()
 filePath = Path(args.file_name)
@@ -16,4 +17,18 @@ filePath = Path(args.file_name)
 def main():
     non_file_name_args = {k: v for k, v in vars(args).items() if k != "file_name" and v not in (None, False)}
     if not non_file_name_args:
-        pprint(global_dictionary["modules_dictionary"][filePath.stem])
+        moduleOutput()
+    else:
+        if args.class_name:
+            classOutput()
+        if args.function_name:
+            functionOutput()
+
+def moduleOutput():
+    pprint(global_dictionary["modules_dictionary"][filePath.stem])
+
+def classOutput():
+    pprint(global_dictionary["classes_dictionary"][args.class_name])
+
+def functionOutput():
+    pass
