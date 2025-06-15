2b15fb553592b268226031571476fcdefe50d41d
HassamGani <hag2137@columbia.edu>
2025-06-14 20:14:13 -0400

    now stores arguments, too

--- separateTester.py
+++ separateTester.py
@@ -1,8 +1,6 @@
 import ast
 
 print(ast.dump(ast.parse("""\
-@decorator1
-@decorator2
-def f(a: annotation, b, c, d, e, f = 2, g = 3) -> 'return annotation':
+def f(self, a: annotation, b, c, d, e, f = 2, g = 3):
     pass
 """), indent=4))
