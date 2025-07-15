832148d56243fba61c3998d618cfbe26a0a1eae9
florayq <floraqiurobot@gmail.com>
2025-07-15 21:56:06 +0100

    automated testing for test1

---
+++ test_module.py
@@ -0,0 +1,27 @@
+import pytest
+import subprocess
+
+@pytest.mark.parametrize("class_str, input_file, expected_file", [
+    ("C", "tests/test1/lib.py", "tests/test1/libC.txt"),
+    ("D", "tests/test1/lib.py", "tests/test1/libD.txt"),
+    ("pinho", "tests/test1/newApp.py", "tests/test1/newApppinho.txt"),
+    ("A", "tests/test1/app.py", "tests/test1/appA.txt"),
+    ("B", "tests/test1/app.py", "tests/test1/appB.txt"),
+    ("new", "tests/test1/app.py", "tests/test1/appnew.txt"),
+]) 
+
+
+def test_process(class_str, input_file, expected_file):
+    result = subprocess.run(
+        ["python3", "analyzer/analyzer.py", "-c", class_str, input_file],
+        capture_output=True,
+        text=True
+    )
+
+    assert result.returncode == 0, f"Script failed: {result.stderr}"
+
+
+    with open(expected_file) as f:
+        expected = f.read()
+
+    assert result.stdout.strip() == expected.strip()
